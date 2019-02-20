6ca765dffddc953ca4e8561ffbd67aff604390da
Muhammad Usama <usama6000@gmail.com>
2019-02-20 15:32:27 +0500

    initial commit

--- api/account/create.js
+++
@@ -1,8 +0,0 @@
-const Account = require('../../models/account/Account');
-
-module.exports = async function(req, res, next) {
-	const {email, name, age} = req.body;
-	const account = new Account({email, name, age});
-	await account.save();
-	return res.send({message: 'success'});
-};
